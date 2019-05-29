d98ad4b3f4806b52a4c664ad7d26abfd63a8d752
jrosbrow <45439168+jrosbrow@users.noreply.github.com>
2019-05-29 19:27:25 -0400

    Bugfix - Polygons now working

--- geojson_repair/repair_utils.py
+++ geojson_repair/repair_utils.py
@@ -1,9 +1,16 @@
 def repair_duplicates(geom):
     """Repair duplicate consecutive coordinates and return the fixed geometry"""
     all_coords = geom.get('geometry').get('coordinates')
-    coords_list = [coords[0] for coords in all_coords]
+    geom_type = geom.get('geometry').get('type')
+    if geom_type == 'MultiPolygon':
+        coords_list = [coords[0] for coords in all_coords]
+    elif geom_type == 'Polygon':
+        coords_list = [coords for coords in all_coords]
     for i, coord_set in enumerate(coords_list):
         fixed_coord_set = [c for n, c in enumerate(coord_set) if n == 0 or c != coord_set[n - 1]]
-        all_coords[i][0] = fixed_coord_set
+        if geom_type == 'MultiPolygon':
+            all_coords[i][0] = fixed_coord_set
+        elif geom_type == 'Polygon':
+            all_coords[i] = fixed_coord_set
     geom['geometry']['coordinates'] = all_coords
     return geom
